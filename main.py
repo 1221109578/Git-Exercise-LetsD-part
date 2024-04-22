a26b6f09193abd99d6600ad40dcbd16eb98e3b23
WAN IRFAN ARIF <julliusbang11@gmail.com>
2024-04-22 22:40:14 +0800

    Main Test

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return
+    return '''<!doctype html>
+    <html>
+    
+    '''
+
+@app.route("/about")
+def about():
+    return "<h1>Hello Death!<h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
