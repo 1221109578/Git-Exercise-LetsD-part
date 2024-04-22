5bc7c0bfdfc9c7d9b93584e266301a0f3808e90c
WAN IRFAN ARIF <julliusbang11@gmail.com>
2024-04-22 23:07:54 +0800

    about page html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return "<h1>Hello Death!<h1>"
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
