640dc66f66dc81abef9819e504cee2f8793ec2a4
WAN IRFAN ARIF <julliusbang11@gmail.com>
2024-04-24 15:21:27 +0800

    Libraries Requirement

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
@@ -10,6 +9,10 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/admin")
+def about():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
