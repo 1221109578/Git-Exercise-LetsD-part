a593a1b0caa66df5fe534229204280417257c9d3
Faris <muhammadfaris.mohdnazri@gmail.com>
2024-04-27 21:58:30 +0800

    added icon on web tab

--- main.py
+++ main.py
@@ -2,17 +2,9 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template('home.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-@app.route("/admin")
-def about():
-    return redirect(url_for("home"))
+    return render_template('Home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
