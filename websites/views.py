622e8378cc1ffdb07506ee5ee248506335f7abed
Amin Rasyid <amin.rasyid.azli@gmail.com>
2024-05-11 23:05:31 +0800

    test css

--- websites/views.py
+++ websites/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect ,url_for
 from flask_login import login_required, current_user
 from . import db 
 
-views = Blueprint('views', __name__)
+views = Blueprint(__name__ , 'views')
 
 @views.route("/", methods=['GET', 'POST'])
 def home():
