650f5b44a882932e94a2dbd65495be8cbd65f421
WAN IRFAN ARIF <julliusbang11@gmail.com>
2024-05-20 16:58:47 +0800

    Nk pull min tk pull tarik

--- websites/views.py
+++ websites/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect ,url_for
 from flask_login import login_required, current_user
 from .__init__ import create_app
+from . import db
 from .models import PaymentMethod
 
 views = Blueprint('views', __name__)
